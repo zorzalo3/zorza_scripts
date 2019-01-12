8b24d8aab3cf3ceadd003076c961c3c0b7bf1627
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2019-01-12 21:33:11 +0100

    Make run.py atomic
    Possibly also improves performance

--- run.py
+++ run.py
@@ -4,16 +4,17 @@ sys.path.append('..')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zorza.settings')
 django.setup()
 
-import import_xml
-# Łączy grupy z planu o tej samej nazwie w jedną (np. językowe)
-import merge_groups
-# Oddziela np. BO od całych klas
-import split_groups
-# Łączy grupy mające lekcję z tym samym nauczycielem w tym samym czasie
-import create_groups
-# Usuwa pozostałości po porzednich skryptach
-import cleanup_groups
-# Zmienia wyświetlane nazwy na bardziej stosowne do wyświetlania
-import rename_subjects
-# Dodaje skrócone lekcje jeśli ich jeszcze nie ma
-import add_shortened_periods
+with django.db.transaction.atomic():
+    import import_xml
+    # Łączy grupy z planu o tej samej nazwie w jedną (np. językowe)
+    import merge_groups
+    # Oddziela np. BO od całych klas
+    import split_groups
+    # Łączy grupy mające lekcję z tym samym nauczycielem w tym samym czasie
+    import create_groups
+    # Usuwa pozostałości po porzednich skryptach
+    import cleanup_groups
+    # Zmienia wyświetlane nazwy na bardziej stosowne do wyświetlania
+    import rename_subjects
+    # Dodaje skrócone lekcje jeśli ich jeszcze nie ma
+    import add_shortened_periods
